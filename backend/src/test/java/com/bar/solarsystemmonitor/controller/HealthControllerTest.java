db4ab8a80fc5ac8ee7356f97a74fd509c892855b
asaf bar <assafbar81@gmail.com>
2024-10-25 14:01:25 +0000

    adding the 1st test

---
+++ backend/src/test/java/com/bar/solarsystemmonitor/controller/HealthControllerTest.java
@@ -0,0 +1,27 @@
+// backend/src/test/java/com/bar/solarsystemmonitor/controller/HealthControllerTest.java
+package com.bar.solarsystemmonitor.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(HealthController.class)
+class HealthControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  void healthCheckShouldReturnOk() throws Exception {
+    mockMvc.perform(get("/health"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string("OK"));
+  }
+}
