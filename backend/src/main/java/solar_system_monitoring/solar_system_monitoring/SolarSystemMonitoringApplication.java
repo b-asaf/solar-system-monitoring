e57b031662ca403bb8017d969d8a3aabc02927ff
asaf bar <assafbar81@gmail.com>
2025-04-08 23:12:26 +0300

    Initial commit: Solar System Monitoring project setup

---
+++ backend/src/main/java/solar_system_monitoring/solar_system_monitoring/SolarSystemMonitoringApplication.java
@@ -0,0 +1,13 @@
+package solar_system_monitoring.solar_system_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarSystemMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarSystemMonitoringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/solar_system_monitoring/solar_system_monitoring/SolarSystemMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package solar_system_monitoring.solar_system_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarSystemMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
